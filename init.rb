7cd69ed232089440c0b6eed68e75eec344bafbbc
holysugar <holysugar@gmail.com>
2014-12-08 22:16:13 +0900

    First implementation

---
+++ init.rb
@@ -0,0 +1,15 @@
+Dir::foreach(File.join(File.dirname(__FILE__), 'lib')) do |file|
+  next unless /\.rb$/ =~ file
+  require file
+end
+
+Rails.application.config.i18n.load_path += Dir["#{File.dirname(__FILE__)}/config/locales/*.{rb,yml}"]
+
+Redmine::Plugin.register :redmine_wiki_extensions_absolute_dates do
+  name 'Redmine Wiki Extensions Absolute Dates plugin'
+  author 'holysugar'
+  description 'Change Redmine Wiki Extensions comment date to absolute date'
+  version '0.0.1'
+  url 'https://github.com/holysugar/redmine_wiki_extensions_absolute_dates'
+  author_url 'https://github.com/holysugar'
+end
---
+++ lib/disable_distance_time_in_words.rb
@@ -0,0 +1,13 @@
+module ActionView
+  module Helpers
+    module DateHelper
+      def distance_of_time_in_words(from_time, to_time = 0, options = {})
+        from_time = from_time.to_time if from_time.respond_to?(:to_time)
+        to_time = to_time.to_time if to_time.respond_to?(:to_time)
+        from_time, to_time = to_time, from_time if from_time > to_time
+
+        I18n.l(from_time.localtime, :format => :absolute)
+      end
+    end
+  end
+end
